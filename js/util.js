9d6e3988c6b5b861f6af237ff33cd645cacf15ed
Liron Kruchinin <54192894+LilKruchi@users.noreply.github.com>
2022-11-24 20:05:00 +0200

    Update util.js

--- js/util.js
+++ js/util.js
@@ -127,3 +127,19 @@ function onCloseModal() {
     var elModal = document.querySelector('.modal');
     elModal.style.display = 'none';
 }
+
+
+function toggleDarkMode(elBtn) {
+    console.log(elBtn);
+
+    var elTheme = document.getElementsByTagName('link')[0]
+
+    if (elTheme.getAttribute('href') == './css/light.css') {
+        elTheme.setAttribute('href', './css/dark.css')
+        elBtn.src = 'img/sun.png'
+    } else {
+        elTheme.setAttribute('href', './css/light.css')
+        elBtn.src = 'img/moon.png'
+    }
+}
+
